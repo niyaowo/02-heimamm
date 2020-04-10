aeaeee57e579fc8d6549110868d887476dccb136
超级英雄 <3085884197@qq.com>
2020-04-10 20:53:28 +0800

    项目初始化

---
+++ src/views/login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="login">
+    <div class="right"></div>
+    <div class="left"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less">
+#login {
+  background: linear-gradient(45edg, #1594f8, #04c6fa);
+  height: 100%;
+}
+</style>
